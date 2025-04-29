2c4dd02a4a8ad926687755c32d6404db7a9d380f
ManalKS5 <139183416+ManalKS5@users.noreply.github.com>
2025-04-29 20:08:55 +0300

    integrate allure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
 
-@Listeners({AllureTestNg.class}) // <== VERY IMPORTANT
+@Listeners({io.qameta.allure.testng.AllureTestNg.class})
 public class BaseTest {
     protected WebDriver driver;
 
